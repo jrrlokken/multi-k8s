fa048dc265d5cefebd00e18f86a1cfb066fb58e3
Joshua Lokken <joshualokken@pm.me>
2021-11-21 09:29:56 -0600

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      I'm Kevin Costner in one of his western roles!
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
