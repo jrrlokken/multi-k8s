c81fe71fff2bffa81715fb3d03cb84459cf9ced7
Joshua Lokken <joshualokken@pm.me>
2021-11-22 13:14:30 -0600

    Update version in banner text

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          {/* Update Version */}
           <h3>*️⃣ Fibonacci Calculator k8s version *️⃣</h3>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
